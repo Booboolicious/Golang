52cd8a42aa59e30bdc045f206a68ca9cec203c22
Booboolicious <101952136+Booboolicious@users.noreply.github.com>
2026-01-19 01:20:04 +0100

    Auto-commit: 2026-01-19 01:20:04

--- Github/Go/05_basic_string_type.go
+++ Github/Go/05_basic_string_type.go
@@ -8,8 +8,9 @@ import (
 func main(){
 	firstName := "John"
 	lastName := "Doe"
-	age := 28.4
-	fullName := fmt.Sprintf("%s-%s %.2f", firstName, lastName, age)
+	age := 28
+	height := 128.4
+	fullName := fmt.Sprintf("%s-%s %d %.2f", firstName, lastName, age, height)
 	fmt.Println(strings.ToUpper(fullName))
 
 }
