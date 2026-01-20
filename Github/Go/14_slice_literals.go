6af94c92de25a7bd49566fd75c8870d2c9c62bf4
Booboolicious <101952136+Booboolicious@users.noreply.github.com>
2026-01-20 14:51:40 +0100

    Auto-commit: 2026-01-20 14:51:40

--- Github/Go/14_slice_literals.go
+++ Github/Go/14_slice_literals.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main(){
 	result := []string{"Ezekiel", "Augustine"}
+	result = append(result, "Abaessien")
 	fmt.Println(result, result[1], result[len(result)-2])
 	result[0] = "Aniema"
 	fmt.Println(result, result[1], result[len(result)-2])
