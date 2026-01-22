bdd4753618b9019e630d09074cd51030ee46765f
Booboolicious <101952136+Booboolicious@users.noreply.github.com>
2026-01-22 12:58:03 +0100

    Auto-commit: 2026-01-22 12:58:03

--- Github/Go/16_for_range_over_slice.go
+++ Github/Go/16_for_range_over_slice.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main (){
-	
+	view := []int {10, 20, 30, 40}
+
+	total := 0
+
+	for index, value := range views {
+		fmt.PrintLn("Day", index, "views", value)
+		total += value
+	}
 }
