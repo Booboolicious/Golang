fe1b3563eeb650e5b4f2b5dcf3ff58060602133e
Booboolicious <101952136+Booboolicious@users.noreply.github.com>
2026-01-22 17:04:10 +0100

    Auto-commit: 2026-01-22 17:04:10

--- Github/Go/19_funtion.go
+++ Github/Go/19_funtion.go
@@ -3,14 +3,16 @@ package main
 import "fmt"
 
 
-func SumAndProduct (a int, b int) (int, int) {
+func SumAndProduct (a int, b int) (int, int) (int, int){
 	sum := a + b
-	product := 
-	return 
+	product := a * b
+	div := a/b
+	return sum, product, div
 }
 
 func main(){
-	sum1 := SumAndProduct(10, 21)
+	sum1, _, _ := SumAndProduct(10, 21)
+	_, product, _ := SumAndProduct(5, 5)
 
-	fmt.Println(sum1)
+	fmt.Println(sum1,product)
 }
