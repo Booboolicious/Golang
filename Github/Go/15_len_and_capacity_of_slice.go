4dc9f3030207fc256d18bb3edc2a140f603b00f4
Booboolicious <101952136+Booboolicious@users.noreply.github.com>
2026-01-22 12:28:03 +0100

    Auto-commit: 2026-01-22 12:28:03

--- Github/Go/15_len_and_capacity_of_slice.go
+++ Github/Go/15_len_and_capacity_of_slice.go
@@ -6,4 +6,12 @@ func main(){
 
 	scores := make([]int, 0,5)
 	fmt.Println(scores, len(scores), cap(scores))
+	
+	scores = append(scores, 100)
+	scores = append(scores, 200, 5000)
+	fmt.Println(scores, len(scores), cap(scores))
+	scores = append(scores, 690, 15000)
+	fmt.Println(scores, len(scores), cap(scores))
+	scores = append(scores, 90, 150)
+	fmt.Println(scores, len(scores), cap(scores))
 }
