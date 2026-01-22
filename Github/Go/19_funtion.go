864837021e672da2dc7d32c0d394f08fa593aa79
Booboolicious <101952136+Booboolicious@users.noreply.github.com>
2026-01-22 17:24:23 +0100

    Auto-commit: 2026-01-22 17:24:23

--- Github/Go/19_funtion.go
+++ Github/Go/19_funtion.go
@@ -13,7 +13,7 @@ func math (a int, b int) (int, int, int){
 func main(){
 	sum1, _, _ := math(10, 21)
 	_, product, _ := math(5, 5)
-	_, _, div := math(5, 5)
+	_, _, div := math(25, 5)
 
-	fmt.Println(sum1,product)
+	fmt.Println(sum1,product, div)
 }
