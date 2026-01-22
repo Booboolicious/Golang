b36c844c2f2fc4b47548829d8fd21128a11a6650
Booboolicious <101952136+Booboolicious@users.noreply.github.com>
2026-01-22 14:53:59 +0100

    Auto-commit: 2026-01-22 14:53:59

--- Github/Go/17_map.go
+++ Github/Go/17_map.go
@@ -4,10 +4,10 @@ import "fmt"
 
 func main (){
 	ages := map[string]int{
-		// map[keyType]
-		"ezekiel": 28
-		"Aniema": 22
+		// map[keyType]valuetype
+		"ezekiel": 28,
+		"Aniema": 22,
 	}
 
-	fmt.Println(ages["ezekiel"])
+	fmt.Println(ages["ezekiel"], len(ages))
 }
