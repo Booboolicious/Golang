a350a60f61457f365ace85fabd7be9ed038ece09
Booboolicious <101952136+Booboolicious@users.noreply.github.com>
2026-01-23 00:50:38 +0100

    Auto-commit: 2026-01-23 00:50:38

--- Github/Go/21_variadic_function.go
+++ Github/Go/21_variadic_function.go
@@ -2,12 +2,17 @@ package main
 
 import "fmt"
 
-func sumAll (nums ...int){
+func sumAll (nums ...int) int {
 
 	total := 0
 
 	for _, val := range nums {
-		total+= val
+		fmt.Println(val)
+		total += val
 	}
 	return total
+}
+
+func main (){
+	fmt.Println(sumAll(1,2,3,5,9,3,4,7))
 }
