5ecd6967531adf91e164b6487eda6da121604c63
Booboolicious <101952136+Booboolicious@users.noreply.github.com>
2026-01-24 12:19:20 +0100

    Auto-commit: 2026-01-24 12:19:20

--- Github/Go/22_error_return_pattern/main.go
+++ Github/Go/22_error_return_pattern/main.go
@@ -2,32 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"strconv"
 )
 
 
 func main(){
 	fmt.Println(`"hey"`)
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
 }
 
 
 func run () error{
-	input := `3`
+	input := "3000"
 
 	level, err := parseLevel(input)
 
 	if err != nil{
 		return err
 	}
-	fmt.Println(`Selected Level`, level)
-	
+	fmt.Println("Selected Level", level)
+	return nil
 }
 
 func parseLevel(s string)(int, error){
 
 	n, err := strconv.Atoi(s)
 	if err != nil {
-		return 404, fmt.Errorf(`level must be a number`)
+		return 0, fmt.Errorf(`level must be a number`)
 	}
 	return  n, nil
 
