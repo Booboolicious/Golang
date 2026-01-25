06ad05a3f19d878ffef8fa4698e61989363f7a90
Booboolicious <101952136+Booboolicious@users.noreply.github.com>
2026-01-25 23:37:05 +0100

    Auto-commit: 2026-01-25 23:37:04

--- Github/Go/23_defer_basics/main.go
+++ Github/Go/23_defer_basics/main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/Booboolicious/Golang/Github/Go/my"
 	"github.com/Booboolicious/Golang/my"
 )
 
 
 func main (){
 	my.Log(`"Case 1: success"`)
-	my.Log.typeof(42)
-	my.Log.typeof("Hello")
+	my.Log.Typeof(42)
+	my.Log.Typeof("Hello")
 	if err := doWork(true); err != nil {
 		my.Log(`error`, err)
 	}
@@ -23,15 +20,15 @@ func main (){
 }
 
 func doWork (success bool) error {
-	log(`"start: resource acquired"`)
-	defer log(`"clean up: resource released"`)
+	my.Log(`"start: resource acquired"`)
+	defer my.Log(`"clean up: resource released"`)
 
 	if !success {
-		return  log.err(`something went wrong, "returning early"`)
+		return  my.Log.Err(`something went wrong, "returning early"`)
 		
 	}
-	log(`work: doing somthing important`)
-	log(`work: It's over`)
+	my.Log(`work: doing somthing important`)
+	my.Log(`work: It's over`)
 	
 	return nil
 }
