5e45d2fa0f486860ea1e6875e29bff2fb393c065
Booboolicious <101952136+Booboolicious@users.noreply.github.com>
2026-01-25 22:26:19 +0100

    Auto-commit: 2026-01-25 22:26:19

--- Github/Go/23_defer_basics/main.go
+++ Github/Go/23_defer_basics/main.go
@@ -6,3 +6,8 @@ import "fmt"
 func main (){
 	fmt.Println(`"hell ya"`)
 }
+
+func doWork (success bool) error {
+	fmt.Println(`"start: resource acquired"`)
+	defer fmt.Println(`"clean up: resource released"`)
+}
